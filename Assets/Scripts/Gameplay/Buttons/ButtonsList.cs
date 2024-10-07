5df9e47aa1794ba4768b912d01367a55203fbc01
Ducquwee <ducquwee@gmail.com>
2024-10-07 19:20:08 +0100

    Added Buttons Valid For Scrambling

--- Assets/Scripts/Gameplay/Buttons/ButtonsList.cs
+++ Assets/Scripts/Gameplay/Buttons/ButtonsList.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
-namespace Buttons    
-{  
-    public class ButtonList : MonoBehaviour
-    {
-        Dictionary<int, KeyCode> buttons = new Dictionary<int, KeyCode>();
+
+public class ButtonList : MonoBehaviour
+{
+    Dictionary<int, KeyCode> buttons = new Dictionary<int, KeyCode>();
+
+    private void Start() {
+        AddButtons();
         
     }
+
+    private void AddButtons() { //Read list of valid buttons from file
+        TextAsset buttonsListTextAsset = Resources.Load<TextAsset>("Valid_Buttons");
+        string ButtonList = buttonsListTextAsset.text;
+
+        for (int i = 0; i < ButtonList.Length; i++) {
+            try {
+                buttons.Add(i, (KeyCode)System.Enum.Parse(typeof(KeyCode), ButtonList[i].ToString()));
+                Debug.Log("Successfully added" + ButtonList[i].ToString());
+            }
+            catch {
+                Debug.LogError("Error loading " + ButtonList[i] + "Is Button invalid type?");
+            }
+        }
+    }
+
 }
+
 
