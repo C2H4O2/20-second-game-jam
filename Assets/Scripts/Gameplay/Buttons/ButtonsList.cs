8895e436e5f514c5f3ae2976fb30dfdc3af17871
Ducquwee <ducquwee@gmail.com>
2024-10-07 18:42:41 +0100

    Added Buttons File structure

---
+++ Assets/Scripts/Gameplay/Buttons/ButtonsList.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Buttons    
+{  
+    public class ButtonList : MonoBehaviour
+    {
+        Dictionary<int, KeyCode> buttons = new Dictionary<int, KeyCode>();
+        
+    }
+}
+
