d60e7157dfb463794952fdb1c5e3c2e15f559c84
C2H4O2 <ngethanyk@gmail.com>
2024-11-18 11:49:28 +0000

    Added the new track to the game

--- Assets/Scripts/UX/Sound/Music/MusicManager.cs
+++ Assets/Scripts/UX/Sound/Music/MusicManager.cs
@@ -1,20 +1,70 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public class Song
+{
+    public string songName;
+    public AudioClip clip;
+}
 
 public class MusicManager : MonoBehaviour
 {
-    [SerializeField] private AudioClip MainMusic;
+    [SerializeField] private List<Song> musicPlaylist;
     private AudioSource audioSource;
+    private List<Song> remainingSongs;
+    private static MusicManager instance;
 
-    void Awake()
+    private void Awake()
     {
-        DontDestroyOnLoad(transform.gameObject);
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        remainingSongs = new List<Song>(musicPlaylist);
     }
 
-    void Start()
+    private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.clip = MainMusic;
-        audioSource.loop = true;
+        PlayNextSong();
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying && audioSource.clip != null)
+        {
+            PlayNextSong();
+        }
+    }
+
+    private void PlayNextSong()
+    {
+        if (remainingSongs.Count == 0)
+        {
+            remainingSongs = new List<Song>(musicPlaylist);
+        }
+
+        Song nextSong = ChooseRandomSong();
+        remainingSongs.Remove(nextSong);
+        Debug.Log($"Now Playing: {nextSong.songName}");
+        audioSource.clip = nextSong.clip;
         audioSource.Play();
+    }
+
+    private Song ChooseRandomSong()
+    {
+        int generatedSongIndex = Random.Range(0, remainingSongs.Count);
+        return remainingSongs[generatedSongIndex];
     }
 }
