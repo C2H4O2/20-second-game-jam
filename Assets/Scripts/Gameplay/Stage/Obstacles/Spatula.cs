0ca00759b52bf24d19fee9e6b525be18a7cfadb3
C2H4O2 <ngethanyk@gmail.com>
2024-12-08 20:42:33 +0000

    Updated Game Balance

--- Assets/Scripts/Gameplay/Stage/Obstacles/Spatula.cs
+++ Assets/Scripts/Gameplay/Stage/Obstacles/Spatula.cs
@@ -7,8 +7,8 @@ public class Spatula : MonoBehaviour
     private Egg egg;
     private Rigidbody2D rb;
     public UnityEvent SpatulaHitEgg;
-    [SerializeField] private float followSpeed = 2f; // Adjust for desired follow speed
-    [SerializeField] private float followSmoothness = 0.05f; // Smoothing for more natural following
+    [SerializeField] private float followSpeed; // Adjust for desired follow speed
+    [SerializeField] private float followSmoothness; // Smoothing for more natural following
     private bool splatulaCanMove;
 
     public bool SplatulaCanMove { get => splatulaCanMove; set => splatulaCanMove = value; }
